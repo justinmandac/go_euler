f5c31d04b0192c97373c43a2bff48018e4471ff7
justinmandac <justinmandac@gmail.com>
2017-03-15 21:20:44 +0800

    solved ex2

--- 02/main.go
+++ 02/main.go
@@ -1,10 +1,26 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Ex02 contains the logic needed to produce the required output for this exercise
 func Ex02(limit int) (result int) {
-	return 0
+	result = 0
+	prev := 1
+	curr := 1
+
+	for i := 1; i < limit; i = prev + curr {
+		prev = curr
+		curr = i
+		if i%2 == 0 {
+			result += i
+		}
+	}
+
+	return result
 }
 
 func main() {
-
+	fmt.Println("Ex02", Ex02(4000000))
 }
